1fcd705c43c457412489d3fa3245aed6cd2ae1a9
Fanjin Zhang <zfjsail@163.com>
2021-06-24 16:06:51 +0800

    try fix

--- CONNA/classifier_main.py
+++ CONNA/classifier_main.py
@@ -689,6 +689,9 @@ if __name__ == '__main__':
             else:
                 ins_num = ins_num - 1
 
+            batch_num = int(batch_num)
+            ins_num = int(ins_num)
+
             tmp_feature = [TE_new_paper_word_ids[batch_num][ins_num * (c_s.TEST_SAMPLE + 1) + c_s.TEST_SAMPLE]]
             tmp_feature = np.repeat(np.array(tmp_feature), c_s.TEST_SAMPLE, axis = 0).tolist()
             _11_new_paper_word_ids.extend(tmp_feature)
